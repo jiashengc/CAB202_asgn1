c04ce32c470370f32aacd6ee43c9f17bb880a90f
Jia Sheng Chong <jiashengc@outlook.com>
2017-08-19 13:17:09 +1000

    draw borders

--- asgn1.c
+++ asgn1.c
@@ -1,20 +1,39 @@
-// gcc ex41.c -std=gnu99 -IZDK -LZDK -lzdk -lncurses -o ex41
+// gcc asgn1.c -std=gnu99 -IZDK -LZDK -lzdk -lncurses -o Casgn1
 #include <stdlib.h>
 #include <math.h>
 #include <cab202_graphics.h>
 #include <cab202_sprites.h>
 #include <cab202_timers.h>
 
-#define DELAY 10;
+#define DELAY 10
+#define SCREEN_WIDTH (screen_width() - 1)
+#define SCREEN_HEIGHT (screen_height() - 1)
 
 bool game_over;
 bool update_screen = true;
 
 sprite_id hero;
 
+char * hero_sprite =
+/**/	" o "
+/**/	"/|\\"
+/**/	"/ \\";
+
+
 void setup(void) {
 
-  
+    // Initialise sprites
+
+
+    // Draw border lines
+    draw_line(0, 1, SCREEN_WIDTH, 1, '-'); // TOP LINE
+    draw_line(0, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, '='); // BOTTOM LINE
+    draw_line(0, 2, 0, SCREEN_HEIGHT - 1, '|'); // LEFT LINE
+    draw_line(SCREEN_WIDTH, 2, SCREEN_WIDTH, SCREEN_HEIGHT - 1, '|'); // RIGHT LINE
+
+}
+
+void process() {
 
 }
 
